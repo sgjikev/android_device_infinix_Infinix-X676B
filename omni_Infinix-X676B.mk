7ddb1b174f0e3ec07a30346d555008e49a42acab
sgjikev <166200226+sgjikev@users.noreply.github.com>
2024-04-06 08:51:36 +0300

    Update omni_Infinix-X676B.mk

--- omni_Infinix-X676B.mk
+++ omni_Infinix-X676B.mk
@@ -13,10 +13,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit from Infinix-X676B device
-$(call inherit-product, device/infinix/Infinix-X676B/device.mk)
+$(call inherit-product, device/infinix/X676B/device.mk)
 
-PRODUCT_DEVICE := Infinix-X676B
-PRODUCT_NAME := omni_Infinix-X676B
+PRODUCT_DEVICE := X676B
+PRODUCT_NAME := omni_X676B
 PRODUCT_BRAND := Infinix
 PRODUCT_MODEL := Infinix X676B
 PRODUCT_MANUFACTURER := infinix
@@ -26,4 +26,4 @@ PRODUCT_GMS_CLIENTID_BASE := android-infinix
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="vext_x676b_h891-user 12 SP1A.210812.016 307761 release-keys"
 
-BUILD_FINGERPRINT := Infinix/X676B-EU/Infinix-X676B:12/SP1A.210812.016/230422V2365:user/release-keys
+BUILD_FINGERPRINT := Infinix/X676B-EU/X676B:12/SP1A.210812.016/230422V2365:user/release-keys
